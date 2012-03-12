670afed7a4c757784771ceab7963ae25d6380189
Neil Sweeney <neil@wolfiezero.com>
2012-03-12 12:54:56 +0000

    Improved DEBUG output structure to allow terminal and HTML views

--- myconn.php
+++ myconn.php
@@ -1,41 +1,44 @@
 <?php
 
 class MyConn {
-	
+
 	var $dbServer;
 	var $dbName;
-	
+
 	var $countRecords;
 	var $arrResults;
 	var $intCurrentAutoID;
-	
+
 	function MyConn($query='', $host=DB_HOST, $user=DB_USER, $pass=DB_PASS, $base=DB_NAME) {
 		$this->dbServer = @mysql_connect($host, $user, $pass) 
 			or die('Server connection not possible.');
-			
+
 		$this->dbName = @mysql_select_db($base, $this->dbServer)
 			or die('Database connection not possible.');
-			
+
 		if( $query ) {
 			$this->setQuery($query);
 		}
 	}
-	
-	function setQuery($query) {
-		
-		$myData = @mysql_query($query, $this->dbServer)
-			or die('MySQL Error: '.mysql_error().'<pre>'.$query.'</pre><br />');
-		
+
+	function setQuery($query, $return=true) {
+
 		if(DEBUG == true) {
-			echo '<pre style="background: #FFFFFF; padding: 2px;">';
-			echo '--MySQL---------------------------------<br />';
-			echo $query.'<br />';
-			echo '----------------------------------------</pre>';
+//			echo '<pre style="background: #FFFFFF; padding: 2px;">';
+			echo '--MySQL---------------------------------'."\n\r";
+//			echo '<br />';
+			echo $query."\n\r";
+//			echo '<br />';
+			echo '----------------------------------------'."\n\r";
+//			echo '</pre>';
 		}
-		
+
+		$myData = @mysql_query($query, $this->dbServer)
+			or die(mailDevs('Issue', 'MySQL Error: '.mysql_error()));
+
 		$this->setCurrentAutoID(mysql_insert_id($this->dbServer));
-		
-		if($myData) {
+
+		if($return && $myData) {
 			$intRows = mysql_num_rows($myData) or 0;
 			$countArr = 0;
 			if($intRows > 0) {
@@ -45,38 +48,43 @@ class MyConn {
 				}
 			}
 		}
-		
-		$this->arrResults = $arrData;
+
+		if (isset($arrData)) {
+			$this->arrResults = $arrData;
+
+		} else {
+			$this->arrResults = false;
+		}
 	}
-	
+
 	function getResults() {
 		return $this->arrResults;
 	}
 	function getNumRecords() {
 		return $this->countRecords;
 	}
-	
+
 	function setCurrentAutoID($intID){
 		$this->intCurrentAutoID = $intID;
 	}
 	function getCurrentAutoID(){
 		return $this->intCurrentAutoID;
 	}
-	
+
 	function setHashIt($strID){
-		
+
 		$arrResults = $this->arrResults;
-		
+
 		$arrData = array();
 		foreach($arrResults as $intID => $arrResult){
 			$arrData[$arrResult[$strID]] = $arrResult;
 		}
-		
+
 		$this->arrResults = $arrData;
 	}
-	
+
 	function makeEntrySafe($strData) {
-		
+
 		#$strData = addslashes($strData);
 		$strData = mysql_real_escape_string($strData);
 		return $strData; //addslashes()
